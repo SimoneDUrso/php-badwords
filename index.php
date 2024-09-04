20fbc0a8208100980beb167ed85aafd4307f298f
SimoneDUrso <simone.durso98@gmail.com>
2024-09-04 15:08:15 +0200

    [UPD] Created textarea and input text

--- index.php
+++ index.php
@@ -1,13 +1,33 @@
-
+<php 
+    
+?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <title>Badwords</title>
 </head>
 <body>
-    
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <form action="./badwords.php" method="GET">
+                    <div class="row">
+                        <div class="col-6">
+                            <label for="paragraph">Paragrafo</label>
+                            <textarea name="paragraph" class="form-control" placeholder="Scrivi qui un messaggio" id="paragraph"></textarea>
+                        </div>
+                        <div class="col-6">
+                            <label for="badword">Parola da censurare nel paragrafo</label>
+                            <input class="form-control w-50" type="text" name="badword" id="badword">
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
