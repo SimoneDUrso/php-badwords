1875b92f55d52a57d8e7f4c826caa9d88db6bb1f
SimoneDUrso <simone.durso98@gmail.com>
2024-09-04 15:35:34 +0200

    [UPD] The paragraph and its length are displayed on the screen

--- badwords.php
+++ badwords.php
@@ -1,11 +1,32 @@
+<?php
+    $paragraph = $_GET['paragraph'];
+    $badword = $_GET['badword'];
+    $paragraphLen = strlen($paragraph);
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <title>Badwords</title>
 </head>
 <body>
-    
+    <div class="container">
+        <div class="row">
+            <div class="col-12 mb-5">
+                <h2 class="text-center fw-bold">Paragrafo con la lunghezza</h2>
+                <p class="text-center fs-3"><span class="fw-bold">Paragrafo:</span> <?php echo $paragraph ?> </p>
+                <p class="text-center fs-3"><span class="fw-bold">Lunghezza paragrafo:</span> <?php echo $paragraphLen ?> </p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12 mt-5">
+                <h2 class="text-center fw-bold">Paragrafo con anche la parola censurata</h2>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
